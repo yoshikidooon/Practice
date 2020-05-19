ad9db9c5d3baddb6fee3a69df0729007c4aad35d
yoshikidooon <yoshiki19990408@docomo.ne.jp>
2020-05-19 22:26:12 +0900

    MenuMaker作成の一部

--- Practice/ViewController.swift
+++ Practice/ViewController.swift
@@ -10,11 +10,74 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var label1: UILabel!
+    @IBOutlet var menuImageView: UIImageView!
+    
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
+    
+    //ボタンを押すとポップアップでラベルにテキストを入力する
+    @IBAction func enterTapped(_ sender: Any) {
+        var alertTextField: UITextField?
+        
 
+        let alert = UIAlertController(
+            title: "Menuを編集する",
+            message: "Menuを追加してください",
+            preferredStyle: UIAlertController.Style.alert)
+        alert.addTextField(
+            configurationHandler: {(textField: UITextField!) in
+                alertTextField = textField
+                textField.text = self.label1.text
+        })
+        alert.addAction(
+            UIAlertAction(
+                title: "Cancel",
+                style: UIAlertAction.Style.cancel,
+                handler: nil))
+        alert.addAction(
+            UIAlertAction(
+                title: "OK",
+                style: UIAlertAction.Style.default) { _ in
+                if let text = alertTextField?.text {
+                    self.label1.text = text
+                }
+            }
+        )
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    
+    
+    //imageを定義した
+    let image = UIImage(named:"menu_design")!
+    
+    //スクリーンショットを撮るメソッド
+    func getScreenShot(windowFrame: CGRect) -> UIImage {
+
+        UIGraphicsBeginImageContextWithOptions(CGSize(width: image.size.width,height: image.size.width), false, 0.0)
+        let context: CGContext = UIGraphicsGetCurrentContext()!
+        self.view.layer.render(in: context)
+        let capturedImage : UIImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+
+        return capturedImage
+       
+    }
+    
+    
+    //スクリーンショットをボタンをタップして実行する
+   @IBAction func tapButton() {
+          getScreenShot(windowFrame: CGRect(x: 0,y: 0,width: image.size.width,height:
+               image.size.width))
+          let image = getScreenShot(windowFrame: self.view.bounds)
+          UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
+      }
+    
+    
 
 }
 
